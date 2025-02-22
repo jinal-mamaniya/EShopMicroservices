0f88480206747c42d9241b53205a5a2ac4543dcc
jinal-mamaniya <jinalmgithub29@gmail.com>
2025-02-22 10:03:32 -0500

    EShop Microservices - Catalog API with CQRS

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,18 @@
+using Carter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+// configure the HTTP request pipeline.
+app.MapCarter();
+
+app.Run();
